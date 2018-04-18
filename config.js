df2f0e05f0010b26bf7fa5df91db836d78236c41
Capi Etheriel <barraponto@gmail.com>
2018-04-18 18:09:59 -0300

    fix(tests): use a different database for tests

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+const testing = process.env.NODE_ENV === 'testing';
+
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/frsr',
+  MONGODB_URL:
+    process.env.MONGODB_URL ||
+    (testing ? 'mongodb://localhost/frsr-test' : 'mongodb://localhost/frsr'),
 };
